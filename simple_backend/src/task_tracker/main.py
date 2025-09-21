55c96eb8d7c26bc21552b14ba0f138b7b473c729
Valerii <valeratk32@list.ru>
2025-09-21 18:50:20 +0300

    Реализация простого бэкенда с хранением задач в оперативной памяти

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,46 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class TaskAdd(BaseModel):
+    name: str
+    status: str
+
+
+tasks = []
+next_id = 1
+
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks
+
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(task: Annotated[TaskAdd, Depends()]):
+    global next_id
+    task["id"] = next_id
+    next_id += 1
+    tasks.append(task)
+    return task
+
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, updated_task: dict):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.update(updated_task)
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"ok": True}
+    raise HTTPException(status_code=404, detail="Task not found")
