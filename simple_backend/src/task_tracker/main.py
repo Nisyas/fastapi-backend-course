d7337f1772c5a12cdc57c547bf5bf5f2a106ea7c
Valerii <valeratk32@list.ru>
2025-09-21 19:21:03 +0300

    Реализация бэкэнда, добавление папки first_venv в .gitignore, добавление README.md файла

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,39 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+tasks = []
+next_id = 1
+
+class AddTask(BaseModel):
+    name: str
+    status: str
 
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(task: Annotated[AddTask, Depends()]):
+    global next_id
+    task["id"] = next_id
+    next_id += 1
+    tasks.append(task)
+    return task
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, updated_task: Annotated[AddTask, Depends()]):
+    for task in tasks:
+        if task["id"] == task_id:
+            task.update(updated_task)   # обновляем поля
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"ok": True}
+    raise HTTPException(status_code=404, detail="Task not found")
