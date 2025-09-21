fe175ce06646063b3324adcc3739b4a1f0c3a854
Valerii <valeratk32@list.ru>
2025-09-21 19:05:39 +0300

    Revert "Реализация простого бэкенда с хранением задач в оперативной памяти"
    This reverts commit 55c96eb8d7c26bc21552b14ba0f138b7b473c729.

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,46 +1,19 @@
-from typing import Annotated
-from fastapi import FastAPI, Depends, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
-
-class TaskAdd(BaseModel):
-    name: str
-    status: str
-
-
-tasks = []
-next_id = 1
-
-
 @app.get("/tasks")
 def get_tasks():
-    return tasks
-
+    pass
 
 @app.post("/tasks")
-def create_task(task: Annotated[TaskAdd, Depends()]):
-    global next_id
-    task["id"] = next_id
-    next_id += 1
-    tasks.append(task)
-    return task
-
+def create_task(task):
+    pass
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, updated_task: dict):
-    for task in tasks:
-        if task["id"] == task_id:
-            tasks.update(updated_task)
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
-
+def update_task(task_id: int):
+    pass
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    for task in tasks:
-        if task["id"] == task_id:
-            tasks.remove(task)
-            return {"ok": True}
-    raise HTTPException(status_code=404, detail="Task not found")
+    pass
